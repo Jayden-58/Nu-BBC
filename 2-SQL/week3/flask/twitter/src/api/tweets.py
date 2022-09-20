d34d872858a3f28b26f2af5c433865404bd1baaf
Jayden-58 <jayden.kellar@gmail.com>
2022-09-19 19:46:06 -0700

    wee

--- 2-SQL/week3/flask/twitter/src/api/tweets.py
+++ 2-SQL/week3/flask/twitter/src/api/tweets.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, request
+from ..models import Tweet, User, db
 
 bp = Blueprint('tweets', __name__, url_prefix='/tweets')
+
+@bp.route('', methods=['GET'])
+def index():
+    tweets = Tweet.query.all()
+    result = []
+    for t in tweets:
+        result.append(t.serialize())
+    return jsonify(result)
